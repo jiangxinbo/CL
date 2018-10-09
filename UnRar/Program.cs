e409ffa2bbbd95969973e23fa31dbac52e336275
jiangxinbo <jiang_tao_2010@163.com>
2018-10-09 20:29:05 +0800

    添加unrar

--- UnRar/Program.cs
+++ UnRar/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace UnRar
 {
@@ -6,6 +7,13 @@ namespace UnRar
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("输入压缩目录");
+            var imgpath = Console.ReadLine();
+            foreach(var filename in Directory.GetFiles(imgpath))
+            {
+                var s = Path.GetFullPath(filename)+Path.GetFileNameWithoutExtension(filename) + ".rar";
+                File.Copy(filename, s);
+            }
             Console.WriteLine("Hello World!");
         }
     }
