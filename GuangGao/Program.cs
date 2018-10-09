6b39edec582a34f8def8c8b32a5f725fb18cfe1d
jiangxinbo <jiang_tao_2010@163.com>
2018-10-09 23:10:55 +0800

    广告图片名称生成

---
+++ GuangGao/Program.cs
@@ -0,0 +1,42 @@
+﻿using Console_DotNetCore_CaoLiu.Bll;
+using Console_DotNetCore_CaoLiu.Tool;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GuangGao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("输入广告图片Web地址");
+
+            var imgurl = Console.ReadLine();
+            //http://oi67.tinypic.com/5p3k7q.jpg
+           
+            var ggname = GetSha1(imgurl) + ".jpg";
+            Console.WriteLine(ggname);
+        }
+
+        public static string GetSha1(string str)
+        {
+            //建立SHA1对象
+            SHA1 sha = new SHA1CryptoServiceProvider();
+
+            //将mystr转换成byte[]
+            ASCIIEncoding enc = new ASCIIEncoding();
+            byte[] dataToHash = enc.GetBytes(str);
+
+            //Hash运算
+            byte[] dataHashed = sha.ComputeHash(dataToHash);
+
+            //将运算结果转换成string
+            return BitConverter.ToString(dataHashed).Replace("-", "").ToLower();
+        }
+    }
+}
